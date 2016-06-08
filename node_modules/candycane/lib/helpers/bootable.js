04ef59a0d001be9d2938c2c5feeb5607763b043b
Ryan Tablada <ryan.tablada@gmail.com>
2016-06-08 15:56:48 -0500

    Ignore node modules

--- node_modules/candycane/lib/helpers/bootable.js
+++
@@ -1,9 +0,0 @@
-export default function(Klass) {
-  return (target) => {
-    target.boot = function (app) {
-      const instance = new Klass(app);
-
-      return instance.createMiddleware();
-    };
-  };
-}
